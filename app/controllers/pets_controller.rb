84feb95413c2255ae93f36fd2f6689b9bce107e2
Branden Mosley <branden.mosley1337@gmail.com>
2018-06-20 18:49:07 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -12,6 +12,11 @@ class PetsController < ApplicationController
 
   post '/pets' do
     binding.pry
+    @pet = Pet.create(params[:pet])
+    if !params["owner"]["name"].empty?
+      @pet.owner << Owner.create(params[:owner])
+    end
+    @pet.save
     redirect to "pets/#{@pet.id}"
   end
 
